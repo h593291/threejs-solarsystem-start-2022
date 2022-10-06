28e16f641d0237acfa2859dbe0ff2328cd75924f
Arne Munthe-Kaas <45269747+h593267@users.noreply.github.com>
2022-10-06 15:24:09 +0200

    Add VR controls

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 "use strict";
 
-import {PerspectiveCamera, Scene, WebGLRenderer} from "./build/three.module.js";
+import {PerspectiveCamera, Scene, WebGLRenderer, Clock} from "./build/three.module.js";
 import SolarSystem from "./solarSystem.js";
 import {VRButton} from "./build/webxr/VRButton.js";
+import {FlyControls} from "./build/FlyControls.js";
 
 export default class App {
 
@@ -35,19 +36,31 @@ export default class App {
 
         this.SolarSystem = new SolarSystem(this.scene);
 
+        this.controls = new FlyControls( this.camera, this.renderer.domElement );
+
+        this.controls.movementSpeed = 1000;
+        this.controls.domElement = this.renderer.domElement;
+        this.controls.rollSpeed = Math.PI / 24;
+        this.controls.autoForward = false;
+        this.controls.dragToLook = false;
+
+        this.clock = new Clock();
 
         document.body.append(VRButton.createButton(this.renderer));
         this.renderer.xr.enabled = true;
         this.renderer.setAnimationLoop(this.render.bind(this));
         //this.render();
-
     }
 
     render() {
+        const delta = this.clock.getDelta();
+
         this.SolarSystem.animate();
         this.renderer.render(this.scene, this.camera);
         //window.requestAnimationFrame(this.render.bind(this));
 
+        this.controls.movementSpeed = 1;
+        this.controls.update( delta );
     }
 
 }
